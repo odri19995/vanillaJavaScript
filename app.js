486f02abd38320d7a68fc7a1ab445981015207c1
odri19995 <odri19995@gmali.com>
2023-02-06 22:51:48 +0900

    toggle 사용

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const h1 =document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const clickedClass = "clicked";
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    }else {
-        h1.classList.add(clickedClass);
-    }// 변수를 잘못 쓴다면 console에서 오류가 출력된다. 
+    
+    h1.classList.toggle("clicked"); //한줄의 코드로 5줄을 대체했다.
+ 
 }
 
 window.addEventListener("click",handleTitleClick); 
 
-/* classList 우리가 class들의 목록으로 작업할수 있게끔 허용해준다.
-className은 이전calss를 상관하지않고 모든걸 교체해 버린다.
+/* 
+toggle은 토큰이 존재하면 토큰제거
+토큰존재 하지않으면 토큰 추가
 
-classList를 이용하는건
-js에서 건드리는건 HTML element가 가지고있는 또하나의 요소 사용하는 것이다.
-= element의 class내용물을 조작하는 것을 허용한다는 뜻
-
-contains은 우리가 명시한 class가 HTML element의 class에 포함되어 있는지 말해준다
+toggle은 h1의 classList에 clicked class가 이미있는지 확인하여
+만약있다면 toggle 이 clicked를 제거해준다
+만약 class name이 존재하지 않는다면 toggle은 classname 추가
+있으면 제거 없으면 생성 !!!
  */
