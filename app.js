712d7af8ffef0df1b92f518a7d8fe67ee6b00db7
odri19995 <odri19995@gmali.com>
2023-02-08 01:36:09 +0900

    Login 5 hidden and string +variable

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
-const link = document.querySelector("a")
-
+const HIDDEN_CLASSNAME = "hidden"; // 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용한다.
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // greeting.innerText ="Hello " + username; 
+    greeting.innerText = `Hello ${username} !`; //둘다 string과 변수를 합쳐주고 있다. ` ` 백틱 기호로 시작 중간에 변수만 추가 가능
+    greeting.classList.remove(HIDDEN_CLASSNAME)//hidden을 두번 사용하는걸 방지하가위해서 상수 선언
 }
 
-function handleLinkClick(event){
-    event.preventDefault(); //동작을 막는다.
-    console.dir(event) //mountEvent가 뜨는걸 확인가능
-    alert("click");
-}
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click",handleLinkClick) //버튼만 주고 실행시키는건 자바스크립트의 몫이다.
-// Js는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어준다. 이 object에는 방금 일어난 event의 정보가 들어있다.
+
 
