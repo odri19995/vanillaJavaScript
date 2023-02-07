d1e58737a931818831e94e2f12230c7199b965fa
odri19995 <odri19995@gmali.com>
2023-02-08 00:58:00 +0900

    Login 4 event link click

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a")
 
-function onLoginSubmit(event){  //js가 argument를 이벤트로 채워준다.
-    event.preventDefault(); //form submit의 기본동작 새로고침 억제
-    console.log(loginInput);
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
+function handleLinkClick(event){
+    event.preventDefault(); //동작을 막는다.
+    console.dir(event) //mountEvent가 뜨는걸 확인가능
+    alert("click");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-// submit event가 발생할때 Js는 onLoginSubmit function을 호출하고 있고 이 때 event object를 argument로 주고있고
+link.addEventListener("click",handleLinkClick) //버튼만 주고 실행시키는건 자바스크립트의 몫이다.
+// Js는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어준다. 이 object에는 방금 일어난 event의 정보가 들어있다.
+
