696c55ae9c7426afb0f030320ecd869d184b3bda
odri19995 <odri19995@gmali.com>
2023-02-09 01:26:10 +0900

    명언 추가Math.random()

---
+++ js/quotes.js
@@ -0,0 +1,51 @@
+const quotes = [
+    {
+      quote: "The way to get started is to quit talking and begin doing.",
+      author: "Walt Disney",
+    },
+    {
+      quote: "Life is what happens when you're busy making other plans.",
+      author: "John Lennon",
+    },
+    {
+      quote:
+        "The world is a book and those who do not travel read only one page.",
+      author: "Saint Augustine",
+    },
+    {
+      quote: "Life is either a daring adventure or nothing at all.",
+      author: "Helen Keller",
+    },
+    {
+      quote: "To Travel is to Live",
+      author: "Hans Christian Andersen",
+    },
+    {
+      quote: "Only a life lived for others is a life worthwhile.",
+      author: "Albert Einstein",
+    },
+    {
+      quote: "You only live once, but if you do it right, once is enough.",
+      author: "Mae West",
+    },
+    {
+      quote: "Never go on trips with anyone you do ntot love.",
+      author: "Hemmingway",
+    },
+    {
+      quote: "We wander for distraction, but we travel for fulfilment.",
+      author: "Hilaire Belloc",
+    },
+    {
+      quote: "Travel expands the mind and fills the gap.",
+      author: "Sheda Savage",
+    },
+  ];
+  
+  const quote = document.querySelector("#quote span:first-child");//quote 의 first span
+  const author = document.querySelector("#quote span:last-child");
+  const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)]; //Array에 랜덤 접근  Math.random() 0부터 1사이에 값 제공 0<=Math.random<1
+  // Math.round = 반올림, Math.ceil(천장) 올림 , Math.floor(바닥 ) : 버림
+  
+  quote.innerText = todaysQuote.quote; //화면에 출력 innerText 명언이 위로 저자가 아래로
+  author.innerText = todaysQuote.author;
