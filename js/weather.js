15d784cf0767759613c59f0cbd698d299d104b14
odri19995 <odri19995@gmali.com>
2023-02-10 02:34:44 +0900

    weather api

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,23 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "4804b91c1f160792d4d94e0beef94aab";
+
+
+
 function onGeoOk(position) {
-    const lat = position.coords.latitude; //위도
-    const lng = position.coords.longitude; //경도
-    console.log("You live in", lat, lng);
-    console.log(position);
-  }
-  function onGeoError() {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;// 이사이트에서 응답한다.
+    console.log(url)
+    fetch(url) //자바스크립트가 url을 부른다.
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
     alert("Can't find you. No weather for you.");
   }
   
