81c38fa622c030164166664293c6a27d48320017
odri19995 <odri19995@gmali.com>
2023-02-09 23:26:22 +0900

    saving todo

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos)); //JSON.stringify 브라우저가 가진 기능 =object나 array나 어떤 것이든 string으로 바꿔줌
+}
+
 function deleteToDo(event) {
-  const li = event.target.parentElement; //parentElement안에는 다른 정보들이 들어간다.  parentElement.innerText하면 안의 값을 알 수 있다.
-  //parent는 li이다.
+  const li = event.target.parentElement;
   li.remove();
 }
 
@@ -12,8 +17,8 @@ function paintToDo(newTodo) {
   const li = document.createElement("li"); 
   const span = document.createElement("span");
   span.innerText = newTodo; 
-  const button =document.createElement("button"); //하지만 어떤 버튼이 눌린지 모른다.
-  button.innerText = "❌"; //윈도우 기준 이모지 윈도우키 + .
+  const button =document.createElement("button");
+  button.innerText = "❌";
   button.addEventListener("click",deleteToDo);
   li.appendChild(span); 
   li.appendChild(button);
@@ -24,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;  
   toDoInput.value = ""; 
-  paintToDo(newTodo); 
+  toDos.push(newTodo); //빈 리스트에 newTodo값을 넣어준다. 하지만 local storage에는 리스트는 저장이 안된다. 단지 텍스트만 저장 가능
+  paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
